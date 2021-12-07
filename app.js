1cc39635600cb4da744eb785ccb5c9fc86adf9f3
unknown <chetanshinde120@gmail.com>
2021-12-07 18:09:44 +0530

    cord change

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 var corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:4000',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
